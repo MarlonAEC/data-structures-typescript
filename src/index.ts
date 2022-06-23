f388c4e74ff4a0c7ccf6fdd6d9d72b27a8bf8526
Marlon Espinosa <marlon9402@outlook.com>
2022-06-22 22:38:20 -0400

    Fix: Corrected import for Linked List (#20)
    * fix: Changelog release notes generator (#15)
    
    * New: Added support for Linked Lists (#16)
    
    * Fix: Corrected Linked List export (#18)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Queue from './Queue';
 import Pair from './Pair';
 import Stack from './Stack';
+import LinkedList from './LinkedList';
+import LinkedListNode from './LinkedList/LinkedListNode';
 
-export { Queue, Pair, Stack };
+export { Queue, Pair, Stack, LinkedList, LinkedListNode };
